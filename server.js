5da8f69721cd5363d51cbf8b8455cb8616cf48c2
kim <s158614@nishitech.ac.jp>
2019-11-13 13:01:03 +0900

    req, res using
    메인 페이지 출력

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 
-const port = 1234;
+const port = 1541;
 
-app.listen(console.log("서버를 시작합니다."));
+app.listen(port ,console.log("서버를 시작합니다."));
 
+app.use((req, res) => {
+    res.json({
+        msg : "홈페이지에 오신걸 환영합니다!"
+    })
+});
