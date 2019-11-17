530f47db924808380f3c7b049765dbaefb78eef9
kim <s158614@nishitech.ac.jp>
2019-11-17 23:01:26 +0900

    Add user Model and C.R

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,27 +1,70 @@
 const express = require("express");
 const router = express.Router();
+const userModel = require("../../models/users");
+
 
 //마이페이지 get
 router.get("/", (req, res) => {
-    res.json({
-        msg : "마이페이지"
-    })
+
+    userModel
+        .find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json({
+                msg : "성공적으로 마이페이지를 불러왔습니다.",
+                count : docs.length,
+                userInfo : docs
+            });
+
+        })
+        .catch(err => {
+            res.status(500).json({
+                error : err.message
+            });
+        });
+
 });
+
+
+
+
+
+
+
 
 //내 정보 등록 post
 router.post("/", (req, res) => {
 
-    const user = {
-        email : req.body.email,
-        password : req.body.password
-    };
+    const user = new userModel({
+       id : req.body.id,
+       password : req.body.password
+    });
 
-    res.json({
-        msg : "내 정보를 등록합니다.",
-        userinfo : user
-    })
+    user
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                msg : "회원가입을 완료했습니다.",
+                userInfo : result
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error : err.message
+            });
+        });
 
 });
+
+
+
+
+
+
+
+
 
 //내 정보 수정 patch or put
 router.put("/", (req, res) => {
@@ -30,11 +73,18 @@ router.put("/", (req, res) => {
     })
 });
 
+
+
+
+
+
 //내 정보 삭제 delete
 router.delete("/", (req, res) => {
     res.json({
         msg : "내 정보를 삭제합니다."
     })
 });
+
+
 
 module.exports = router;
